21371562ed81c41ca3af801074e8cfb05ef2b4e0
Esko Dijk <esko.dijk@iotconsultancy.nl>
2025-11-20 14:14:41 +0100

    (Please squash this commit with previous ones.)
    Resolve license header conflict by applying OT license. Minor import reshuffling to alphabetical order.

--- tools/tcat_ble_client/dataset/dataset_entries.py
+++ tools/tcat_ble_client/dataset/dataset_entries.py
@@ -1,19 +1,4 @@
 """
-   Copyright (c) 2023 Nordic Semiconductor ASA
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-
   Copyright (c) 2025, The OpenThread Authors.
   All rights reserved.
 
@@ -39,13 +24,12 @@
   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
   POSSIBILITY OF SUCH DAMAGE.
-
 """
 
-import struct
-import inspect
-from typing import List
 from abc import ABC, abstractmethod
+import inspect
+import struct
+from typing import List
 
 from tlv.dataset_tlv import MeshcopTlvType
 from tlv.tlv import TLV
